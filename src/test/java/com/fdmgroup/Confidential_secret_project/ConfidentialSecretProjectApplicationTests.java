143c24fec750081c205434822c63e782b4eb55ce
Maciej Gomulec <maciej.gomulec@fdmgroup.com>
2023-02-13 15:03:31 +0100

    first commit

---
+++ src/test/java/com/fdmgroup/Confidential_secret_project/ConfidentialSecretProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fdmgroup.Confidential_secret_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfidentialSecretProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
